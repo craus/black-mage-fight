6e34b0904f6f02825ace3c8da0ceb7a4d67292af
craus <craus902@gmail.com>
2019-11-15 13:36:01 +0400

    fix mobile buttons size; fix mobile buttons layout; mobile buttons are now fast (ondown, not onup)

---
+++ Assets/Scripts/UI/InstantButton.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+using RSG;
+using System;
+using System.Collections.Generic;
+using UnityEngine.Events;
+using UnityEngine.EventSystems;
+
+public class InstantButton : MonoBehaviour, IPointerDownHandler
+{
+	public UnityEvent onMouseDown;
+
+	public void OnPointerDown(PointerEventData eventData)
+	{
+		onMouseDown.Invoke();
+	}
+}
