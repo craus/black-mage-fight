c62b228addd29524a7be2be61768244a9da6a583
craus <craus902@gmail.com>
2019-11-22 13:22:44 +0400

    monster hit sound and animation

---
+++ Assets/Scripts/Animations/ShowForTime.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+using UnityEngine.Events;
+
+public class ShowForTime : MonoBehaviour
+{
+	public GameObject target;
+	public float time;
+	public UnityEvent afterStart;
+	public UnityEvent then;
+	public bool makeCopy;
+
+	public void Run() {
+		if (!makeCopy) {
+			target.SetActive(true);
+			afterStart.Invoke();
+			TimeManager.Wait(time).Then(() => {
+				target.SetActive(false);
+				then.Invoke();
+			});
+		} else {
+			var copy = Instantiate(target);
+			copy.transform.SetParent(target.transform.parent, false);
+			copy.transform.SetParent(null, true);
+			copy.SetActive(true);			
+			afterStart.Invoke();
+			TimeManager.Wait(time).Then(() => {
+				Destroy(copy);
+				then.Invoke();
+			});
+		}
+	}
+}
